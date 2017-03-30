c0975e16c11c92d0963f6f3059bb5c975363dced
joseova <jdovalle@uninorte.edu.co>
2017-03-30 08:31:23 -0500

    Version con SplashScreen

---
+++ src/Gerente/Gerent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gerente;
+
+import Cocina.Cocina;
+
+/**
+ *
+ * @author tepes
+ */
+public class Gerent {
+      public static void main(String[] args) {
+       Gerente a = new Gerente();
+       a.setVisible(true);
+       Cocina b = new Cocina();
+       b.setVisible(true);
+    }
+}
